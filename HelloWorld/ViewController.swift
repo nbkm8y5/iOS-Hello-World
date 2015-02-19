5d361683a05f45f03c559b59acee0cc82bc79863
nbkm8y5 <moreno.ronline@gmail.com>
2015-02-19 15:33:30 -0500

    iOS First App
    Hello World!

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Rolando Moreno on 2/19/15.
+//  Copyright (c) 2015 Rolando Moreno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func showMessage() {
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello Natacha!  This will cost you three fiddy!", preferredStyle: UIAlertControllerStyle.Alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+}
+
